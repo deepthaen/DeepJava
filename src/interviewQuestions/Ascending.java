b7c59aabfbfabc5127e734b4de9b9d8cafc37fff
deeptha <deepthaen@gmail.com>
2019-04-22 08:03:30 +0530

    ascending descending

--- src/interviewQuestions/Ascending.java
+++ src/interviewQuestions/Ascending.java
@@ -1,9 +1,39 @@
 package interviewQuestions;
 
+import java.util.Scanner;
+
 public class Ascending {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		int temp;
+		Scanner s = new Scanner(System.in);
+		System.out.println("Enter array range");
+		int n = s.nextInt();
+		int a[] = new int[n];
+		System.out.println("Enter array values");
+		for (int i = 0 ;  i < n; i++) {
+			a[i] = s.nextInt();
+			
+		}
+		for(int i = 0 ; i<n ;i++) {
+			for (int j = i+1; j< n; j++) {
+				if(a[i]>a[j]) {
+					temp = a[i];
+					a[i] = a[j];
+					a[j] = temp ;
+					
+
+			}
+			}
+		}
+		System.out.println("Ascending order of array");
+		for(int i =0; i<n-1; i++) {
+			System.out.print(a[i]+ ",");
+			
+		}
+		System.out.println(a[n-1]);
 
 	}
 
--- src/interviewQuestions/Descending.java
+++ src/interviewQuestions/Descending.java
@@ -1,5 +1,35 @@
 package interviewQuestions;
 
-public class Descending {
+import java.util.Scanner;
 
+public class Descending {
+	public static void main(String[] args) {
+		int temp;
+		Scanner s = new Scanner(System.in);
+		System.out.println("Enter array range");
+		int n = s.nextInt();
+		int a[] = new int[n];
+		System.out.println("Enter array values");
+		for(int i =0 ; i< n; i++) {
+			a[i] = s.nextInt();
+			}
+		
+		for(int i = 0; i<n; i++) {
+			for(int j = i+1 ; j<n; j++) {
+				if(a[i]< a[j]) {
+					temp =a[i];
+				a[i] = a[j];
+				a[j] = temp;
+			}
+		}
+	}
+		System.out.println("Descending order of array");
+		for (int i = 0; i< n-1; i++) {
+			System.out.print(a[i]+ ",");
+			
+		}
+		System.out.println(a[n-1]);
+	
+
+}
 }
